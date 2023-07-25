7e9ac8cff8ecea14433331e22dd1235100e4f6a8
Bartlomiej Noworyta <101003585+B4JD1K@users.noreply.github.com>
2023-07-25 23:32:36 +0200

    made logic of project

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+import {Column} from "../../models/column.model";
+import {Board} from "../../models/board.model";
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
   styleUrls: ['./main-view.component.scss']
 })
-export class MainViewComponent {
+export class MainViewComponent implements OnInit {
 
+  constructor() {
+  }
+
+  board:Board = new Board('Test Board',[
+    new Column('Ideas',[
+      "Idea no 1",
+      "Idea no 1",
+      "Idea no 1",
+    ]),
+    new Column('Research',[
+      "Research no 1",
+      "Research no 1",
+      "Research no 1",
+    ]),
+    new Column('Todo',[
+      "Todo no 1",
+      "Todo no 1",
+      "Todo no 1",
+    ]),
+    new Column('Done',[
+      "Done no 1",
+      "Done no 1",
+      "Done no 1",
+    ])
+  ])
+
+  ngOnInit(): void {
+  }
+
+  // ideas = [
+  //   'Task nr 1',
+  //   'Task nr 2',
+  //   'Task nr 3',
+  //   'Task nr 4',
+  //   'Task nr 5',
+  // ]
+  //
+  // research = [
+  //   'Task nr A',
+  //   'Task nr B',
+  //   'Task nr C',
+  //   'Task nr D',
+  //   'Task nr E',
+  //   'Task nr F',
+  // ]
+  //
+  // todo = [
+  //   'Task nr A',
+  //   'Task nr B',
+  //   'Task nr C',
+  //   'Task nr D',
+  //   'Task nr E',
+  //   'Task nr F',
+  // ]
+  //
+  // done = [
+  //   'Task nr A',
+  //   'Task nr B',
+  //   'Task nr C',
+  //   'Task nr D',
+  //   'Task nr E',
+  //   'Task nr F',
+  // ]
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex)
+    }
+  }
 }
